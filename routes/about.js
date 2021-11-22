e6f277b2021578b404b91711d6175d5a270c4612
yashas Bhadregowda <yashu.gsb@gmail.com>
2021-11-22 13:21:21 -0500

    updating title

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* About page router. */
 router.get('/', function(req, res, next) {
-  res.render('about', { title: 'Portfolio' });
+  res.render('about', { title: 'yashas portfolio' });
 });
 
 module.exports = router;
