664fadf1f8e2d0efb7cf42a6bf5eddbdb1fefeac
yashas Bhadregowda <yashu.gsb@gmail.com>
2021-11-21 20:09:46 -0500

    updating project file

--- routes/about.js
+++ routes/about.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* About page router. */
 router.get('/', function(req, res, next) {
   res.render('about', { title: 'Portfolio' });
 });
--- routes/projects.js
+++ routes/projects.js
@@ -1,20 +1,24 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios')
-/* GET home page. */
+
+
+
+/* Projects page router. */
 
 router.get('/', function(req, res, next) {
+  axios.get('https://api.github.com/users/yashas1/repos?per_page=6&sort=asc&client_id=7709cf7c5fd2b86e2b5c&client_secret=1888471bddf43a1d4778b958626b0c8aa4daed97')
+  .then(response => {
 
+    res.render('projects', { getGetHubData: response.data });
+  })
+  .catch(error => {
+    console.log(error);
+  });
   
-  const getGetHubData = async () => {
-    try {
-      return await axios.get('https://api.github.com/users/yashas1/repos?per_page=6&sort=asc&client_id=7709cf7c5fd2b86e2b5c&client_secret=1888471bddf43a1d4778b958626b0c8aa4daed97')
-    } catch (error) {
-      console.error(error)
-    }
-  }
-  console.log(typeof getGetHubData)
-  res.render('projects', { getGetHubData: getGetHubData });
+
+ 
+  
 });
 
 module.exports = router;
